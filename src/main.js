cbaab52272da64b923068987798f770a34dea9e2
drumstix42 <drumstix42@gmail.com>
2022-01-28 15:52:26 -0500

    Remove primevue, add naive-ui

--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import PrimeVue from 'primevue/config';
-import PrimeButton from 'primevue/button';
-import PrimeCard from 'primevue/card';
-import PrimeTabView from 'primevue/tabview';
-import PrimeTabPanel from 'primevue/tabpanel';
-
-import 'primevue/resources/primevue.min.css';
-// https://primefaces.org/primevue/showcase/#/setup -> Styles
-import 'primevue/resources/themes/arya-blue/theme.css';
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
-
 import '@/style/style.scss';
 
 import App from '@/App.vue';
@@ -22,11 +10,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(PrimeVue);
-
-app.component('PButton', PrimeButton);
-app.component('PCard', PrimeCard);
-app.component('PTabView', PrimeTabView);
-app.component('PTabPanel', PrimeTabPanel);
 
 app.mount('#app');
