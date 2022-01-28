abb45f13e8499a65e4f89bb183a16b8a6a0ddeef
drumstix42 <drumstix42@gmail.com>
2022-01-28 14:28:46 -0500

    Add ItemSelector empty component

---
+++ src/pages/Icarus/CraftingTool/CraftingCalculator/ItemSelector.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'CraftingToolItemSelector',
+    components: {},
+    props: {},
+    data() {
+        return {};
+    },
+    computed: {},
+    methods: {},
+};
+</script>
