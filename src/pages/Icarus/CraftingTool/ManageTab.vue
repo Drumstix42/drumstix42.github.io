aa83d3ce6e43d98598c992f022c97e0339f060db
drumstix42 <drumstix42@gmail.com>
2022-01-28 13:51:52 -0500

    Delete tab logic, styling

--- src/pages/Icarus/CraftingTool/ManageTab.vue
+++ src/pages/Icarus/CraftingTool/ManageTab.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex p-2" :style="{ backgroundColor: 'var(--gray-700)' }">
+    <div class="manage-tab flex p-2" :style="{ backgroundColor: 'var(--gray-700)' }">
         <div class="flex-grow-1 flex align-items-center">
             <!-- <div>{{ tab.id }}</div> -->
             <div><input v-model.lazy="tab.title" /></div>
         </div>
-        <p-button class="p-button-danger" @click="removeTab(tab.id)">
+        <p-button v-if="tabCount > 1" class="p-button-danger" @click="removeTab(tab.id)">
             <span class="pi pi-times"></span>
         </p-button>
     </div>
@@ -27,10 +27,16 @@ export default {
         return {};
     },
     computed: {
-        ...mapState(useIcarusStore, ['tabs']),
+        ...mapState(useIcarusStore, ['tabs', 'tabCount']),
     },
     methods: {
         ...mapActions(useIcarusStore, ['removeTab']),
     },
 };
 </script>
+
+<style scoped lang="scss">
+.manage-tab {
+    min-height: 3.5rem;
+}
+</style>
