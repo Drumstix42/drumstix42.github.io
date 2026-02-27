ea88648e90cb95e28589567ff36a4832e33aaae3
Drumstix42 <drumstix42@gmail.com>
2026-02-27 00:02:54 -0500

    Update UE4 export script for UE4_27 compatibility
    Add UE4_27 compatibility and usage note to scripts/Ue4ExportFiles/export.bat. Includes a comment noting the tested Ue4Export v4.2.1, passes the 'UE4_27' engine flag to Ue4Export.exe calls so exports match Icarus (UE4_27), adds a pause at the end, and fixes minor newline/rmdir formatting.

--- scripts/Ue4ExportFiles/export.bat
+++ scripts/Ue4ExportFiles/export.bat
@@ -1,7 +1,11 @@
 @echo off
 :: Please note the filepath needs to be updated to your game installation folder
-Ue4Export.exe "C:\Games\Steam\steamapps\common\Icarus\Icarus\Content\Paks" assetlist.txt export
-Ue4Export.exe "C:\Games\Steam\steamapps\common\Icarus\Icarus\Content\Data" assetlist_data.txt export/data
+:: Tested with Ue4Export v4.2.1 (https://github.com/CrystalFerrai/Ue4Export/releases)
+:: Icarus uses UE4_27
+Ue4Export.exe "C:\Games\Steam\steamapps\common\Icarus\Icarus\Content\Paks" UE4_27 assetlist.txt export
+Ue4Export.exe "C:\Games\Steam\steamapps\common\Icarus\Icarus\Content\Data" UE4_27 assetlist_data.txt export/data
 
 for /D %%G in (export\data\*) do move %%G export
 rmdir export\data
+
+pause
